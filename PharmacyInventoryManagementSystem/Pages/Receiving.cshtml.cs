7434af1b19f31aacce0ee09431fd18f4648f76d7
red_ant <mugishathierry65@gmail.com>
2023-05-01 18:27:03 +0200

    created template files

---
+++ PharmacyInventoryManagementSystem/Pages/Receiving.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PharmacyInventoryManagementSystem.Pages
+{
+	public class ReceivingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
