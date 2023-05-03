b9baabd890d5d15e1d481da3db0d85c32950abab
red_ant <mugishathierry65@gmail.com>
2023-05-03 18:29:50 +0200

    can create and view medicine type and category

---
+++ PharmacyInventoryManagementSystem/Pages/Medicines/ManageMedicineCategory.cshtml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace PharmacyInventoryManagementSystem.Pages.Medicines
+{
+	public class ManageMedicineCategoryModel : PageModel
+    {
+        public MedicinesCategories category = new MedicinesCategories();
+        public List<MedicinesCategories> listCategories = new List<MedicinesCategories>();
+
+        string connstring = @"server=localhost;user=root;password=mugishathi;database=pharmacyInventoryMgtSystem_db";
+
+        public string errors = "";
+        public string success = "";
+
+        public void OnPost()
+        {
+            category.category_name = Request.Form["category_name"];
+
+            using (MySqlConnection conn = new MySqlConnection(connstring))
+            {
+                // Insert query
+                String query = "INSERT INTO tbl_medicine_category (category_name, created_by) VALUES(@category_name, @creator_id)";
+
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@category_name", category.category_name);
+                    cmd.Parameters.AddWithValue("@creator_id", 2);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected >= 1)
+                    {
+                        Response.Redirect("/Medicines/ManageMedicineCategory/");
+                    }
+                }
+            }
+        }
+        public void OnGet()
+        {
+            MySqlConnection conn = null;
+
+            using (conn = new MySqlConnection(connstring))
+            {
+                conn.Open();
+                String qry = "SELECT category_id, category_name, created_date, full_name FROM tbl_medicine_category JOIN tbl_user ON tbl_user.user_id = tbl_medicine_category.created_by";
+                using (MySqlCommand cmd = new MySqlCommand(qry, conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MedicinesCategories categoryToSee = new MedicinesCategories();
+
+                            categoryToSee.category_id = "" + reader.GetString(0);
+                            categoryToSee.category_name = reader.GetString(1);
+                            categoryToSee.created_date = reader.GetString(2);
+                            categoryToSee.creator = reader.GetString(3);
+
+                            listCategories.Add(categoryToSee);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public class MedicinesCategories
+    {
+        public string category_id;
+        public string category_name;
+        public string created_date;
+        public string creator;
+    }
+}
---
+++ PharmacyInventoryManagementSystem/Pages/Medicines/ManageMedicineType.cshtml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+
+namespace PharmacyInventoryManagementSystem.Pages.Medicines
+{
+	public class ManageMedicineTypeModel : PageModel
+    {
+        public MedicinesListTypes type = new MedicinesListTypes();
+        public List<MedicinesListTypes> listTypes = new List<MedicinesListTypes>();
+
+        string connstring = @"server=localhost;user=root;password=mugishathi;database=pharmacyInventoryMgtSystem_db";
+
+        public string errors = "";
+        public string success = "";
+
+        public void OnPost()
+        {
+            type.type_name = Request.Form["type_name"];
+
+            using (MySqlConnection conn = new MySqlConnection(connstring))
+            {
+                // Insert query
+                String query = "INSERT INTO tbl_medicine_type (type_name, created_by) VALUES(@type_name, @creator_id)";
+
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@type_name", type.type_name);
+                    cmd.Parameters.AddWithValue("@creator_id", 2);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected >= 1)
+                    {
+                        Response.Redirect("/Medicines/ManageMedicineType");
+                    }
+                }
+            }
+        }
+        public void OnGet()
+        {
+            MySqlConnection conn = null;
+
+            using (conn = new MySqlConnection(connstring))
+            {
+                conn.Open();
+                String qry = "SELECT type_id, type_name, created_date, full_name FROM tbl_medicine_type JOIN tbl_user ON tbl_user.user_id = tbl_medicine_type.created_by";
+                using (MySqlCommand cmd = new MySqlCommand(qry, conn))
+                {
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MedicinesListTypes typeToSee = new MedicinesListTypes();
+
+                            typeToSee.type_id = "" + reader.GetString(0);
+                            typeToSee.type_name = reader.GetString(1);
+                            typeToSee.created_date = reader.GetString(2);
+                            typeToSee.creator = reader.GetString(3);
+
+                            listTypes.Add(typeToSee);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public class MedicinesListTypes
+    {
+        public string type_id;
+        public string type_name;
+        public string created_date;
+        public string creator;
+    }
+}
