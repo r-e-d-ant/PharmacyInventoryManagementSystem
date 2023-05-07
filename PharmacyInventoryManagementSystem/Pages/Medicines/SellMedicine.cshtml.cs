c65b9619c7528430e2276923fcf4f93072ae7a65
red_ant <mugishathierry65@gmail.com>
2023-05-07 21:49:54 +0200

    prepared page for selling medicines

---
+++ PharmacyInventoryManagementSystem/Pages/Medicines/SellMedicine.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PharmacyInventoryManagementSystem.Pages.Medicines
+{
+	public class SellMedicineModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
